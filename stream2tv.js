09c834d1b3ae3df8bc706d639bdf3603068fbfa9
BuildTools <unconfigured@null.spigotmc.org>
2016-08-10 00:53:02 +0300

    Fix subtitles issue due to a recent upnp-mediarenderer-client update

--- stream2tv.js
+++ stream2tv.js
@@ -31,7 +31,7 @@ streamserver.on('request', function (req, res) {
 		res.writeHead(200, { "Content-Type": "text/html" });
 		var fileS = filename_o.replace(/\.[^/.]+$/, ".srt");
 		fs.readFile(fileS, "binary", function(err, fileS) {
-			if(err) {        
+			if(err) {
 				res.writeHead(500, {"Content-Type": "text/plain"});
 				res.write(err + "\n");
 				res.end();
@@ -73,51 +73,16 @@ streamserver.on('connection', function (socket) {
 
 streamserver.listen(settings.localPort);
 
-metadata = xmlb.create('DIDL-Lite', {
-		'headless': true
-	})
-	.att({
-		'xmlns': 'urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/',
-		'xmlns:dc': 'http://purl.org/dc/elements/1.1/',
-		'xmlns:upnp': 'urn:schemas-upnp-org:metadata-1-0/upnp/',
-		'xmlns:dlna': 'urn:schemas-dlna-org:metadata-1-0/',
-		'xmlns:sec': 'http://www.sec.co.kr/',
-		'xmlns:xbmc': 'urn:schemas-xbmc-org:metadata-1-0/'
-	})
-	.ele('item', {
-		'id': '0',
-		'parentID': '-1',
-		'restricted': '1'
-	})
-	.ele('dc:title', {}, 'Stream2TV')
-	.insertAfter('res', {
-		'protocolInfo': 'http-get:*:video/mp4:*',
-		'xmlns:pv': 'http://www.pv.com/pvns/',
-		'pv:subtitleFileUri': url_subtitle,
-		'pv:subtitleFileType': 'srt'
-	}, url_video)
-	.insertAfter('res', {
-		'protocolInfo': 'http-get:*:text/srt:'
-	}, url_subtitle)
-	.insertAfter('res', {
-		'protocolInfo': 'http-get:*:smi/caption'
-	}, url_subtitle)
-	.insertAfter('sec:CaptionInfoEx', {
-		'sec:type': 'srt'
-	}, url_subtitle)
-	.insertAfter('sec:CaptionInfo', {
-		'sec:type': 'srt'
-	}, url_subtitle)
-	.insertAfter('upnp:class', {}, 'object.item.videoItem.movie')
-	.end({
-		pretty: false
-	});
-
 var options = { 
 	autoplay: true,
-	metadata: metadata
+	metadata: {
+		subtitlesUrl: url_subtitle,
+		url: url_video,
+		type: "video",
+		title: 'Stream2TV'
+	},
+	contentType: "video/mp4"
 };
-
 var browser = new Browser();
 
 browser.onDevice(function (device) {
